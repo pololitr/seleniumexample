e5e859628c3fd28ea6660e70355136d261602c6e
Stepan Chmel <stepan.chmel@gmail.com>
2020-01-03 00:25:59 +0100

    DRY without the D, project creation

--- src/test/java/cz/vse/selenium/testCreateProject.java
+++ src/test/java/cz/vse/selenium/testCreateProject.java
@@ -1,20 +1,154 @@
 package cz.vse.selenium;
 
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+import java.util.UUID;
+
+
 public class testCreateProject {
+    UUID uuid = UUID.randomUUID();
+    String randomUUIDString = uuid.toString();
     /*
+    Test Suite - Creating project
 
-    TC#1: (Precondition - there exists project yourname already in the system.)
-    New Task will be created with type Task, name, status New, prio Medium and some description.
-    Verify task attributes (Type Task, description, name, priority, status) on task info page (icon i). Delete that task.
+   TC#2: Project with status New, priority High and filled start date as today is created.
+         Verify that there is new row in project table.
+         Delete project after test.
 
-    TC#2: (Precondition - there exists project yourname already in the system.)
-    Create new 7 tasks with different statuses New, Open, Waiting, Done, Closed, Paid, Canceled.
-    Verify that using default filter (New, Open, Waiting) only 3 tasks will be shown.
-    Change applied filter in Filter info dialog to only contain (New, Waiting) ...there are more ways how to do it
-    (you can click small x on Open "label" to delete it, or you can deal with writing into "suggestion box").
-    Verify only New and Waiting tasks are displayed. Now remove all filters and verify all created tasks are displayed.
-    Delete all tasks using Select all and batch delete.
+   TC#1: Project without name is not created
 
 
      */
+
+
+    private ChromeDriver driver;
+
+    @Before
+    public void init() {
+        ChromeOptions cho = new ChromeOptions();
+        boolean runOnTravis = false;
+        if (runOnTravis) {
+            cho.addArguments("headless");
+        } else {
+            System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
+        }
+//      ChromeDriverService service = new ChromeDriverService();
+        driver = new ChromeDriver(cho);
+//      driver.manage().window().maximize();
+    }
+
+    @After
+    public void tearDown() {
+        // driver.close();
+    }
+
+    @Test
+    public void createProjectPositive() {
+        // given
+        driver.get("https://digitalnizena.cz/rukovoditel/");
+
+        // when
+        WebElement usernameInput = driver.findElement(By.name("username"));
+        usernameInput.sendKeys("rukovoditel");
+        WebElement passwordInput = driver.findElement(By.name("password"));
+        passwordInput.sendKeys("vse456ru");
+        WebElement loginButton = driver.findElement(By.xpath("//button[@type=\"submit\" and @class=\"btn btn-info pull-right\"]"));
+        loginButton.click();
+
+        //go to projects
+        WebElement sidebarProjects = driver.findElement(By.xpath("//i[@class=\"fa fa-reorder\"]"));
+        sidebarProjects.click();
+
+       //add project
+        WebElement addProject = driver.findElement(By.xpath("//button[@class=\"btn btn-primary\" and @type=\"button\"]"));
+        addProject.click();
+
+        //Project priority
+        WebDriverWait wait1 = new WebDriverWait(driver, 5);
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("fields_156")));
+        Select priority = new Select(driver.findElement(By.xpath("//select[@name=\"fields[156]\"]")));
+        priority.selectByVisibleText("Urgent");
+        priority.selectByValue("35");
+
+        //Project status
+        Select projectStatus = new Select(driver.findElement(By.id("fields_157")));
+        projectStatus.selectByValue("37");
+
+
+        //Project Name
+
+        WebElement projectName = driver.findElement(By.xpath("//input[@name=\"fields[158]\"]"));
+        projectName.sendKeys("chms00-"+ randomUUIDString);
+
+        //Project start date
+        WebElement projectDate = driver.findElement(By.xpath("//button[@class=\"btn btn-default date-set\"]"));
+        projectDate.click();
+
+        //Project start date set today
+        WebElement projectDateSetToday = driver.findElement(By.xpath("//td[@class=\"active day\"]"));
+        projectDate.click();
+
+        WebElement projectSubmit = driver.findElement(By.xpath("//button[@type=\"submit\" and @class=\"btn btn-primary btn-primary-modal-action\"]"));
+        projectSubmit.click();
+
+    }
+
+    @Test
+    public void createProjectNegative() {
+        // given
+        driver.get("https://digitalnizena.cz/rukovoditel/");
+
+        // when
+        WebElement usernameInput = driver.findElement(By.name("username"));
+        usernameInput.sendKeys("rukovoditel");
+        WebElement passwordInput = driver.findElement(By.name("password"));
+        passwordInput.sendKeys("vse456ru");
+        WebElement loginButton = driver.findElement(By.xpath("//button[@type=\"submit\" and @class=\"btn btn-info pull-right\"]"));
+        loginButton.click();
+
+        //go to projects
+        WebElement sidebarProjects = driver.findElement(By.xpath("//i[@class=\"fa fa-reorder\"]"));
+        sidebarProjects.click();
+
+        //add project
+        WebElement addProject = driver.findElement(By.xpath("//button[@class=\"btn btn-primary\" and @type=\"button\"]"));
+        addProject.click();
+
+        //Project priority
+        WebDriverWait wait1 = new WebDriverWait(driver, 5);
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.id("fields_156")));
+        Select priority = new Select(driver.findElement(By.xpath("//select[@name=\"fields[156]\"]")));
+        priority.selectByVisibleText("Urgent");
+        priority.selectByValue("35");
+
+        //Project status
+        Select projectStatus = new Select(driver.findElement(By.id("fields_157")));
+        projectStatus.selectByValue("37");
+
+
+        //Project Name
+        WebElement projectName = driver.findElement(By.xpath("//input[@name=\"fields[158]\"]"));
+        projectName.sendKeys("");
+
+        //Project start date
+        WebElement projectDate = driver.findElement(By.xpath("//button[@class=\"btn btn-default date-set\"]"));
+        projectDate.click();
+
+        //Project start date set today
+        WebElement projectDateSetToday = driver.findElement(By.xpath("//td[@class=\"active day\"]"));
+        projectDate.click();
+
+        WebElement projectSubmit = driver.findElement(By.xpath("//button[@type=\"submit\" and @class=\"btn btn-primary btn-primary-modal-action\"]"));
+        projectSubmit.click();
+
+        WebElement required = driver.findElement(By.xpath("//label[@id=\"fields_158-error\"]"));
+    }
 }
